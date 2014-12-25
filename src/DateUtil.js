254e86c913b0626d28bf86f217846e0dac35d83a
Garston Tremblay <garstont+github@gmail.com>
2014-12-24 22:22:54 -0700

    Bringing in RentNotifier DateUtil methods

--- src/DateUtil.js
+++ src/DateUtil.js
@@ -1,9 +1,15 @@
 DateUtil = {
-    addDays: function (days, date){
-        return new Date(date.getFullYear(), date.getMonth(), date.getDate() + days);
+    addDays: function(days, date) {
+        var date = new Date(date);
+        date.setDate(date.getDate() + days);
+        return date;
     },
 
-    prettyDate: function (ts){
+    dayDiff: function(date1, date2) {
+        return Math.floor((date2 - date1) / 86400000);
+    },
+
+    prettyDate: function(ts) {
         return (ts.getMonth()+1) + '/' + ts.getDate() + '/' + ts.getFullYear();
     },
 
@@ -14,5 +20,11 @@ DateUtil = {
             day: parts[1],
             year: parts[2]
         };
+    },
+
+    startOfDay: function(date) {
+        var date = new Date(date);
+        date.setHours(0, 0, 0, 0);
+        return date;
     }
 };
