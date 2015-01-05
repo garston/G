72b2c6527a16027de20daaa2b191503ad2cda933
Garston Tremblay <garstont+github@gmail.com>
2015-01-04 17:24:29 -0700

    Add git sha to built output

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-(cd ../JSUtil && ./build.sh) && cat ../JSUtil/build/all.js src/* > build/all.js
+echo "//++++++++++++++++++++++++++++++++RentNotifier `git rev-parse HEAD`++++++++++++++++++++++++++++++++" > build/all.js
+(cd ../JSUtil && ./build.sh) && cat ../JSUtil/build/all.js src/* >> build/all.js
+echo "//--------------------------------RentNotifier `git rev-parse HEAD`--------------------------------" >> build/all.js
