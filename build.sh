58bf21e94c436710bbca6a880fb7eccedfe9ec11
Garston Tremblay <garstont+github@gmail.com>
2015-02-07 17:22:18 -0700

    Renaming project to LordGarston

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "//++++++++++++++++++++++++++++++++RentNotifier `git rev-parse HEAD`++++++++++++++++++++++++++++++++" > build/all.js
+echo "//+++++++++++++++++++++++++++++++++LordGarston `git rev-parse HEAD`+++++++++++++++++++++++++++++++++" > build/all.js
 (cd ../JSUtil && ./build.sh) && cat ../JSUtil/build/all.js src/* >> build/all.js
-echo "//--------------------------------RentNotifier `git rev-parse HEAD`--------------------------------" >> build/all.js
+echo "//---------------------------------LordGarston `git rev-parse HEAD`---------------------------------" >> build/all.js
