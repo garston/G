4e15ad363a08c8bda5ea4760f0139025fc79c474
Garston Tremblay <garstont+github@gmail.com>
2015-02-19 16:40:41 -0700

    Moving build logic to separate repo

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "//+++++++++++++++++++++++++++++++++LordGarston `git rev-parse HEAD`+++++++++++++++++++++++++++++++++" > build/all.js
-(cd ../JSUtil && ./build.sh) && cat ../JSUtil/build/all.js src/* >> build/all.js
-echo "//---------------------------------LordGarston `git rev-parse HEAD`---------------------------------" >> build/all.js
