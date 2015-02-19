358f068423245450999b6b762f53c0eb87945ee8
Garston Tremblay <garstont+github@gmail.com>
2015-02-19 16:40:22 -0700

    Moving build logic to separate repo

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "//+++++++++++++++++++++++++++++++++++PhysEd `git rev-parse HEAD`+++++++++++++++++++++++++++++++++++" > build/all.js
-(cd ../JSUtil && ./build.sh) && cat ../JSUtil/build/all.js src/* >> build/all.js
-echo "//-----------------------------------PhysEd `git rev-parse HEAD`-----------------------------------" >> build/all.js
