098a0501f2c3dca465988cbf92a6e9de4cbd54c9
Garston Tremblay <garstont+github@gmail.com>
2015-02-20 17:04:02 -0700

    Initial commit

---
+++ src/MailSender.js
@@ -0,0 +1,38 @@
+MailSender = {};
+
+MailSender.getNameUsedForSending = function() {
+    return SpreadsheetApp.getActiveSpreadsheet().getName();
+};
+
+MailSender.forward = function(message, email, body) {
+    message.forward(this._getEmail(email), this._getOptions(body, email));
+};
+
+MailSender.replyAll = function(thread, body, replyTo){
+    if(PROD_MODE){
+        thread.replyAll(body, this._getOptions(body, replyTo));
+    }else{
+        this.send('test replyAll', body);
+    }
+};
+
+MailSender.send = function(subject, body, email){
+    MailApp.sendEmail(this._getEmail(email), subject, this._getPlainBody(body), this._getOptions(body, email));
+};
+
+MailSender._getEmail = function(email){
+    return PROD_MODE ? email : Session.getActiveUser().getEmail();
+};
+
+MailSender._getOptions = function(body, replyTo){
+    return {
+        bcc: Session.getActiveUser().getEmail(),
+        htmlBody: body,
+        name: this.getNameUsedForSending(),
+        replyTo: replyTo
+    };
+};
+
+MailSender._getPlainBody = function(body){
+    return body.replace(/(<([^>]+)>)/ig, '');
+};
