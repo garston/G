d8d1bfe7c8883c041c6231256a644a2576c3003d
Garston Tremblay <garstont+github@gmail.com>
2015-02-25 17:04:36 -0700

    Notify what was processed and what wasn't

--- src/0.js
+++ src/0.js
@@ -1,26 +1,43 @@
 HalfZs = {};
 
 function checkChaseTransactions() {
+    var processedStrings = [];
+    var notProcessedStrings = [];
+
     JSUtil.ArrayUtil.forEach(GmailApp.search('label:' + HalfZs.Const.CHASE_LABEL), function(thread){
         JSUtil.ArrayUtil.forEach(thread.getMessages(), function(message){
             if(!message.isInTrash()){
                 var transactionInfo = /([0-9]+[.][0-9][0-9]) at (.+) has been authorized on 0?([0-9]+)[/][0-9]+[/]([0-9]+)/.exec(message.getPlainBody());
+                var amount = transactionInfo[1];
+                var chaseFullName = transactionInfo[2];
+                var month = transactionInfo[3];
+                var year = transactionInfo[4];
 
                 var sharingInfo = JSUtil.ArrayUtil.find(HalfZs.Const.SHARING_INFOS, function(sharingInfo){
-                    return JSUtil.StringUtil.contains(transactionInfo[2].toLowerCase(), (sharingInfo.chaseName || sharingInfo.prettyName).toLowerCase());
+                    return JSUtil.StringUtil.contains(chaseFullName.toLowerCase(), (sharingInfo.chaseName || sharingInfo.prettyName).toLowerCase());
                 });
                 if(sharingInfo){
                     var sharedTransaction = GASton.Database.hydrateBy(HalfZs.SharedTransaction, ['month', '']);
-                    sharedTransaction.month = transactionInfo[3];
-                    sharedTransaction.year = transactionInfo[4];
+                    sharedTransaction.month = month;
+                    sharedTransaction.year = year;
                     sharedTransaction.what = sharingInfo.prettyName;
-                    sharedTransaction.iPayed = transactionInfo[1];
+                    sharedTransaction.iPayed = amount;
                     sharedTransaction.percentOwed = sharingInfo.splitPercent;
                     GASton.Database.persist(HalfZs.SharedTransaction, sharedTransaction);
 
-                    message.moveToTrash();
+                    processedStrings.push([month, year, sharingInfo.prettyName, '$' + amount, sharingInfo.splitPercent + '%'].join(' '));
+                }else{
+                    notProcessedStrings.push([month, year, chaseFullName, '$' + amount].join(' '));
                 }
+
+                message.moveToTrash();
             }
         });
     });
+
+    if(processedStrings.length || notProcessedStrings.length){
+        GASton.MailSender.sendToIndividual('HalfZs ' + JSUtil.DateUtil.toPrettyString(new Date()), [
+            'Processed:', processedStrings.join('<br/>'), '<br/>', 'Not Processed:', notProcessedStrings.join('<br/>')
+        ].join('<br/>'), Session.getActiveUser().getEmail());
+    }
 }
