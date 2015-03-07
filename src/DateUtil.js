df96dfedc69389b726d8331e53f464fdf67965e9
Garston Tremblay <garstont+github@gmail.com>
2015-03-06 20:40:04 -0700

    Adding DateUtil.lastDayOfMonth()

--- src/DateUtil.js
+++ src/DateUtil.js
@@ -1,12 +1,18 @@
 JSUtil.DateUtil = {
     addDays: function(days, date) {
-        var date = new Date(date);
-        date.setDate(date.getDate() + days);
-        return date;
+        var newDate = new Date(date);
+        newDate.setDate(newDate.getDate() + days);
+        return newDate;
     },
 
     dayDiff: function(date1, date2) {
         return Math.floor((date2 - date1) / 86400000);
+    },
+
+    lastDayOfMonth: function(date) {
+        var newDate = new Date(date);
+        newDate.setFullYear(newDate.getFullYear(), newDate.getMonth() + 1, 0)
+        return newDate;
     },
 
     splitPrettyDate: function(prettyDate){
@@ -19,9 +25,9 @@ JSUtil.DateUtil = {
     },
 
     startOfDay: function(date) {
-        var date = new Date(date);
-        date.setHours(0, 0, 0, 0);
-        return date;
+        var newDate = new Date(date);
+        newDate.setHours(0, 0, 0, 0);
+        return newDate;
     },
 
     toPrettyString: function(date) {
