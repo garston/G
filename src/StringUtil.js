f3a8acf70e7db8b96428b3f8af66db42b659cec6
Garston Tremblay <garstont+github@gmail.com>
2015-08-14 18:03:27 -0600

    Adding StringUtil.capitalize()

--- src/StringUtil.js
+++ src/StringUtil.js
@@ -1,4 +1,8 @@
 JSUtil.StringUtil = {
+    capitalize: function(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+
     contains: function(str, subStr){
         return str.indexOf(subStr) > -1;
     }
