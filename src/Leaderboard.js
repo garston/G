7d9080bcae52aa9b09215b438386339962850247
Garston Tremblay <garstont+github@gmail.com>
2015-10-03 12:31:26 -0600

    Combining leaderboard header text and value into one column array

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -9,35 +9,74 @@ PhysEd.Leaderboard.getLeaderboard = function(sportName, personSports, boldPlayer
     streakablePropsColorMap[PhysEd.PersonSport.STREAKABLE_PROPS.WINS] = streakablePropsColorMap[PhysEd.PersonSport.STREAKABLE_PROPS.INS] = 'red';
     streakablePropsColorMap[PhysEd.PersonSport.STREAKABLE_PROPS.LOSSES] = streakablePropsColorMap[PhysEd.PersonSport.STREAKABLE_PROPS.OUTS] = 'blue';
 
+    var columns = [
+        {
+            header: 'Name',
+            getValue: function(personSport){return personSport.getPerson().getDisplayString();}
+        },
+        {
+            header: 'Win %',
+            getValue: function(personSport){return personSport.getWinPercentage() + '%';}
+        },
+        {
+            header: 'Wins',
+            getValue: function(personSport){return personSport[PhysEd.PersonSport.STREAKABLE_PROPS.WINS];}
+        },
+        {
+            header: 'Losses',
+            getValue: function(personSport){return personSport[PhysEd.PersonSport.STREAKABLE_PROPS.LOSSES];}
+        },
+        {
+            header: 'Ties',
+            getValue: function(personSport){return personSport[PhysEd.PersonSport.STREAKABLE_PROPS.TIES];}
+        },
+        {
+            header: 'Win streak',
+            getValue: function(personSport){
+                return {
+                    color: streakablePropsColorMap[personSport.streakDir],
+                    html: personSport.streak + personSport.streakDir[0].toUpperCase()
+                };
+            }
+        },
+        {
+            header: '+/-',
+            getValue: function(personSport){return (personSport.plusMinus > 0 ? '+' : '') + personSport.plusMinus;}
+        },
+        {
+            header: 'Participation %',
+            getValue: function(personSport){return personSport.getParticipationPercentage() + '%';}
+        },
+        {
+            header: 'Ins',
+            getValue: function(personSport){return personSport[PhysEd.PersonSport.STREAKABLE_PROPS.INS];}
+        },
+        {
+            header: 'Outs',
+            getValue: function(personSport){return personSport[PhysEd.PersonSport.STREAKABLE_PROPS.OUTS];}
+        },
+        {
+            header: 'Participation streak',
+            getValue: function(personSport){
+                return {
+                    color: streakablePropsColorMap[personSport.participationStreakDir],
+                    html: personSport.participationStreak + personSport.participationStreakDir
+                };
+            }
+        },
+    ];
+
     return 'Leaderboard for ' + sportName + '<br/>' +
         '<table>' +
             '<tr>' +
-                JSUtil.ArrayUtil.map(['Name', 'Win %', 'Wins', 'Losses', 'Ties', 'Win streak', '+/-', 'Participation %', 'Ins', 'Outs', 'Participation streak'], function(header){
-                    return '<th>' + header + '</th>';
+                JSUtil.ArrayUtil.map(columns, function(column){
+                    return '<th>' + column.header + '</th>';
                 }).join('') +
             '</tr>' +
             JSUtil.ArrayUtil.map(personSports, function(personSport){
-                var rowItems = [
-                    personSport.getPerson().getDisplayString(),
-                    personSport.getWinPercentage() + '%',
-                    personSport[PhysEd.PersonSport.STREAKABLE_PROPS.WINS],
-                    personSport[PhysEd.PersonSport.STREAKABLE_PROPS.LOSSES],
-                    personSport[PhysEd.PersonSport.STREAKABLE_PROPS.TIES],
-                    {
-                        color: streakablePropsColorMap[personSport.streakDir],
-                        html: personSport.streak + personSport.streakDir[0].toUpperCase()
-                    },
-                    (personSport.plusMinus > 0 ? '+' : '') + personSport.plusMinus,
-                    personSport.getParticipationPercentage() + '%',
-                    personSport[PhysEd.PersonSport.STREAKABLE_PROPS.INS],
-                    personSport[PhysEd.PersonSport.STREAKABLE_PROPS.OUTS],
-                    {
-                        color: streakablePropsColorMap[personSport.participationStreakDir],
-                        html: personSport.participationStreak + personSport.participationStreakDir
-                    }
-                ];
                 return '<tr' + (JSUtil.ArrayUtil.contains(boldPlayerEmails, personSport.getPerson().email) ? ' style="font-weight: bold;"' : '') + '>' +
-                        JSUtil.ArrayUtil.map(rowItems, function(rowItem){
+                        JSUtil.ArrayUtil.map(columns, function(column){
+                            var rowItem = column.getValue(personSport);
                             return '<td' + (rowItem.color ? ' style="color: ' + rowItem.color + ';"' : '') + '>' +
                                     (rowItem.html || rowItem) +
                                 '</td>';
