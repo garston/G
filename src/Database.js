f643b00f4860d6c0052c1b07cf3ed568622e94eb
Garston Tremblay <garstont+github@gmail.com>
2015-12-24 20:41:42 -0700

    Replacing all Database reads with a more performant hydrate(...) that hydrates all objects

--- src/Database.js
+++ src/Database.js
@@ -1,43 +1,19 @@
 GASton.Database = {};
 
-GASton.Database.getLastRow = function(clazz){
-    return this._getSheet(clazz).getLastRow();
-};
-
-GASton.Database.hasObject = function(clazz, colNameValuePairs){
-    return !!this._getRowNumBy(clazz, colNameValuePairs);
-};
-
-GASton.Database.hydrate = function(clazz, guid){
-    return this.hydrateBy(clazz, ['guid', guid]);
-};
-
-GASton.Database.hydrateAll = function(clazz){
-    return JSUtil.ArrayUtil.range(this._getFirstRow(clazz), this.getLastRow(clazz) + 1).map(function(row){
-        return this._hydrateRow(clazz, row);
-    }, this);
-};
-
-GASton.Database.hydrateAllBy = function(clazz, colNameValuePairs){
-    var records = [];
-
-    var startRow = this._getFirstRow(clazz);
-    while(true){
-        var o = this.hydrateBy(clazz, colNameValuePairs, startRow);
-        if(o){
-            records.push(o);
-            startRow = o.__row + 1;
-        }else{
-            return records;
-        }
-    }
-};
-
-GASton.Database.hydrateBy = function(clazz, colNameValuePairs, _startRow){
-    var row = this._getRowNumBy(clazz, colNameValuePairs, _startRow);
-    if(row){
-        return this._hydrateRow(clazz, row);
-    }
+GASton.Database.hydrate = function(clazz){
+    var firstRow = this._getClassMetadataValue(clazz, '__firstRow') || 1;
+    return this._getSheet(clazz).getDataRange().getValues().
+        filter(function(rowData, rowIndex){ return rowIndex + 1 >= firstRow; }).
+        map(function(rowData, index){
+            var o = new clazz();
+            o.__row = index + firstRow;
+            this._getClassMetadataValue(clazz, '__props').forEach(function(prop, propIndex){
+                if(prop){
+                    o[prop] = rowData[propIndex];
+                }
+            });
+            return o;
+        }, this);
 };
 
 GASton.Database.persist = function(clazz, o){
@@ -62,51 +38,12 @@ GASton.Database.remove = function(clazz, o){
     }
 };
 
-GASton.Database._getCell = function(clazz, row, colName){
-    return this._getSheet(clazz).getRange(row, this._getClassMetadataValue(clazz, '__props').indexOf(colName) + 1);
-};
-
-GASton.Database._getCellValue = function(clazz, row, colName){
-    return this._getCell(clazz, row, colName).getValue();
-};
-
-GASton.Database._getFirstRow = function(clazz){
-    return this._getClassMetadataValue(clazz, '__firstRow') || 1;
-};
-
 GASton.Database._getClassMetadataValue = function(clazz, fieldName) {
     return typeof(clazz[fieldName]) === 'function' ? clazz[fieldName].call(clazz) : clazz[fieldName];
 };
 
-GASton.Database._getClassPropsMetadataManagedPropsOnly = function(clazz) {
-    return this._getClassMetadataValue(clazz, '__props').filter(function(prop){ return prop; });
-};
-
-GASton.Database._getRowNumBy = function(clazz, colNameValuePairs, startRow){
-    return JSUtil.ArrayUtil.find(JSUtil.ArrayUtil.range(startRow || this._getFirstRow(clazz), this.getLastRow(clazz) + 1), function(row){
-        for(var nameValPair = 0; nameValPair < colNameValuePairs.length; nameValPair += 2){
-            var colName = colNameValuePairs[nameValPair];
-            var expectedValue = colNameValuePairs[nameValPair + 1];
-            var actualValue = this._getCellValue(clazz, row, colName);
-            if(expectedValue !== actualValue){
-                return false;
-            }
-        }
-        return true;
-    }, this);
-};
-
 GASton.Database._getSheet = function(clazz){
     return SpreadsheetApp.getActiveSpreadsheet().getSheetByName(this._getClassMetadataValue(clazz, '__tableName'));
-};
-
-GASton.Database._hydrateRow = function(clazz, row){
-    var o = new clazz();
-    o.__row = row;
-    this._getClassPropsMetadataManagedPropsOnly(clazz).forEach(function(prop){
-        o[prop] = this._getCellValue(clazz, row, prop);
-    }, this);
-    return o;
 };
 
 GASton.Database._persistNew = function(clazz, o){
@@ -121,14 +58,14 @@ GASton.Database._persistNew = function(clazz, o){
 
 GASton.Database._persistProperty = function(clazz, o, property){
     if(GASton.PROD_MODE){
-        this._getCell(clazz, o.__row, property).setValue(o[property]);
+        this._getSheet(clazz).getRange(o.__row, this._getClassMetadataValue(clazz, '__props').indexOf(property) + 1).setValue(o[property]);
     } else {
         Logger.log('UPDATE %s:%s - %s: %s', this._getClassMetadataValue(clazz, '__tableName'), o.__row, property, o[property]);
     }
 };
 
 GASton.Database._persistUpdateAllProperties = function(clazz, o){
-    this._getClassPropsMetadataManagedPropsOnly(clazz).forEach(function(property){
+    this._getClassMetadataValue(clazz, '__props').filter(function(prop){ return prop; }).forEach(function(property){
         this._persistProperty(clazz, o, property);
     }, this);
 };
