e57d511fa16df9bf25c9a2fe61dc69af9e80cb2c
Garston Tremblay <garstont+github@gmail.com>
2016-10-24 12:39:12 -0600

    Adding Database cache

--- src/Database.js
+++ src/Database.js
@@ -1,12 +1,11 @@
 GASton.Database = {};
+GASton.Database._cache = {};
 
 GASton.Database.hydrate = function(clazz){
-    var firstRow = this._getClassMetadataValue(clazz, '__firstRow') || 1;
-    return this._getSheet(clazz).getDataRange().getValues().
-        filter(function(rowData, rowIndex){ return rowIndex + 1 >= firstRow; }).
-        map(function(rowData, index){
+    this._cache[clazz] = this._cache[clazz] || this._getSheet(clazz).getDataRange().getValues().
+        filter(function(rowData, rowIndex){ return rowIndex + 1 >= this._getFirstRow(clazz); }, this).
+        map(function(rowData){
             var o = new clazz();
-            o.__row = index + firstRow;
             this._getClassMetadataValue(clazz, '__props').forEach(function(prop, propIndex){
                 if(prop){
                     o[prop] = rowData[propIndex];
@@ -14,10 +13,11 @@ GASton.Database.hydrate = function(clazz){
             });
             return o;
         }, this);
+    return this._cache[clazz];
 };
 
 GASton.Database.persist = function(clazz, o){
-    if(o.__row){
+    if(this._cache[clazz] && JSUtil.ArrayUtil.contains(this._cache[clazz], o)){
         this._persistUpdateAllProperties(clazz, o);
     }else{
         this._persistNew(clazz, o);
@@ -32,23 +32,22 @@ GASton.Database.persistOnly = function(clazz, o, properties){
 
 GASton.Database.remove = function(clazz, o){
     if(GASton.PROD_MODE){
-        this._getSheet(clazz).deleteRow(o.__row);
+        this._getSheet(clazz).deleteRow(this._getRowIndex(clazz, o));
     } else {
-        Logger.log('DELETE %s:%s', this._getClassMetadataValue(clazz, '__tableName'), o.__row);
+        Logger.log('DELETE %s:%s', this._getClassMetadataValue(clazz, '__tableName'), this._getRowIndex(clazz, o));
     }
+    JSUtil.ArrayUtil.remove(this._cache[clazz], o);
 };
 
-GASton.Database._getClassMetadataValue = function(clazz, fieldName) {
-    return typeof(clazz[fieldName]) === 'function' ? clazz[fieldName].call(clazz) : clazz[fieldName];
-};
-
-GASton.Database._getSheet = function(clazz){
-    return SpreadsheetApp.getActiveSpreadsheet().getSheetByName(this._getClassMetadataValue(clazz, '__tableName'));
-};
+GASton.Database._getClassMetadataValue = function(clazz, fieldName) { return typeof(clazz[fieldName]) === 'function' ? clazz[fieldName].call(clazz) : clazz[fieldName]; };
+GASton.Database._getFirstRow = function(clazz){ return this._getClassMetadataValue(clazz, '__firstRow') || 1; };
+GASton.Database._getRowIndex = function(clazz, o){ return this._cache[clazz].indexOf(o) + this._getFirstRow(clazz); };
+GASton.Database._getSheet = function(clazz){ return SpreadsheetApp.getActiveSpreadsheet().getSheetByName(this._getClassMetadataValue(clazz, '__tableName')); };
 
 GASton.Database._persistNew = function(clazz, o){
-    var newRow = this._getClassMetadataValue(clazz, '__props').map(function(prop){ return prop ? o[prop] : ''; });
+    this.hydrate(clazz).push(o);
 
+    var newRow = this._getClassMetadataValue(clazz, '__props').map(function(prop){ return prop ? o[prop] : ''; });
     if(GASton.PROD_MODE){
         this._getSheet(clazz).appendRow(newRow);
     } else {
@@ -58,14 +57,14 @@ GASton.Database._persistNew = function(clazz, o){
 
 GASton.Database._persistProperty = function(clazz, o, property){
     if(GASton.PROD_MODE){
-        this._getSheet(clazz).getRange(o.__row, this._getClassMetadataValue(clazz, '__props').indexOf(property) + 1).setValue(o[property]);
+        this._getSheet(clazz).getRange(this._getRowIndex(clazz, o), this._getClassMetadataValue(clazz, '__props').indexOf(property) + 1).setValue(o[property]);
     } else {
-        Logger.log('UPDATE %s:%s - %s: %s', this._getClassMetadataValue(clazz, '__tableName'), o.__row, property, o[property]);
+        Logger.log('UPDATE %s:%s - %s: %s', this._getClassMetadataValue(clazz, '__tableName'), this._getRowIndex(clazz, o), property, o[property]);
     }
 };
 
 GASton.Database._persistUpdateAllProperties = function(clazz, o){
-    this._getClassMetadataValue(clazz, '__props').filter(function(prop){ return prop; }).forEach(function(property){
+    JSUtil.ArrayUtil.compact(this._getClassMetadataValue(clazz, '__props')).forEach(function(property){
         this._persistProperty(clazz, o, property);
     }, this);
 };
