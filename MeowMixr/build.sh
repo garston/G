2e3e6b110b91b40c2869423de2e19693b02fcc64
Garston Tremblay <garstont+github@gmail.com>
2016-11-16 15:14:20 -0700

    Prepare for repo merge

--- MeowMixr/build.sh
+++ MeowMixr/build.sh
@@ -4,5 +4,5 @@ REPO_NAME=${PWD##*/}
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 echo "//+++++++++++++++++++++++++++++++++++$REPO_NAME `git rev-parse HEAD`+++++++++++++++++++++++++++++++++++" > $SCRIPT_DIR/build/$REPO_NAME.txt
-cat src/* >> $SCRIPT_DIR/build/$REPO_NAME.txt
+cat * >> $SCRIPT_DIR/build/$REPO_NAME.txt
 echo "//-----------------------------------$REPO_NAME `git rev-parse HEAD`-----------------------------------" >> $SCRIPT_DIR/build/$REPO_NAME.txt
