b7859c2f5752dc87b16b563f39f435abcb690d6e
Garston Tremblay <garstont+github@gmail.com>
2016-12-27 15:21:37 -0700

    Removing incorrect GASton.Voice assumptions about the existence of a number in missed call emails and that all texting numbers start with 1

--- GASton/Voice.js
+++ GASton/Voice.js
@@ -3,12 +3,15 @@ GASton.Voice.DOMAIN = 'txt.voice.google.com';
 GASton.Voice.SUBJECT = 'SMS';
 
 GASton.Voice.forwardTxt = function(message, text, gvNumber, number, gvKey){
-    GASton.Mail.forward(message, text, '1' + gvNumber + '.1' + number + '.' + gvKey + '@' + this.DOMAIN);
+    GASton.Mail.forward(message, text, '1' + gvNumber + '.' + (number.toString().length === 10 ? 1 : '') + number + '.' + gvKey + '@' + this.DOMAIN);
 };
 
-GASton.Voice.getMissedCallNumber = function(message){ return message.getBody().match(/\((\d+)\) (\d+)-(\d+)/).slice(1).join(''); };
+GASton.Voice.getMissedCallNumber = function(message){
+    var match = message.getBody().match(/\((\d+)\) (\d+)-(\d+)/);
+    return match && match.slice(1).join('');
+};
 
 GASton.Voice.parseFromTxt = function(message){
-    var match = GASton.Mail.parseFrom(message).email.match(/^\d+\.1(\d+)\.(.+)@/);
-    return { gvKey: match[2], number: parseInt(match[1]) };
+    var match = GASton.Mail.parseFrom(message).email.match(/^\d+\.(\d+)\.(.+)@/);
+    return { gvKey: match[2], number: parseInt(match[1].substring(match[1].length === 11 ? 1 : 0)) };
 };
