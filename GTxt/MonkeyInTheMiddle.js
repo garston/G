eb0a463b1cfe4b89ad99014c004605ac12ac3dab
Garston Tremblay <garstont+github@gmail.com>
2018-09-17 13:52:46 -0600

    [GTxt] move GM and VM notations into metadata section

--- GTxt/MonkeyInTheMiddle.js
+++ GTxt/MonkeyInTheMiddle.js
@@ -4,23 +4,29 @@ GTxt.MonkeyInTheMiddle.forwardTexts = function(config) {
     GTxt.ReceiverMonkey.txtPhysicalPhone(this._processTxtEmails(
         'from:' + GTxt.Voice.TXT_DOMAIN + ' subject:' + GTxt.Voice.TXT_SUBJECT,
         function(message){ return GTxt.Voice.parseFromTxt(message).number; },
-        function(message){ return GTxt.Voice.getTxt(message); },
+        function(message){
+            var txt = GTxt.Voice.getTxt(message);
+            return txt === 'MMS Received' ? '' : txt;
+        },
+        function(message){ return message.getAttachments().length ? 'MMS' : ''; },
         config,
         GTxt.Voice.isNotMarketing
     ).concat(this._processTxtEmails(
         'from:' + GTxt.Voice.NO_REPLY_EMAIL + ' subject:' + GTxt.Voice.GROUP_TXT_SUBJECT,
         function(message){ return GTxt.Voice.getFirstNumberMentioned(message.getSubject()); },
+        function(){ return ''; },
         function(){ return 'GM'; },
         config
     )).concat(this._processTxtEmails(
         'from:' + GTxt.Voice.NO_REPLY_EMAIL + ' subject:' + GTxt.Voice.VOICEMAIL_SUBJECT,
         function(message){ return GTxt.Voice.getVoicemailFrom(message); },
-        function(message){ return 'VM: ' + GTxt.Voice.getVoicemailText(message); },
+        function(message){ return GTxt.Voice.getVoicemailText(message); },
+        function(){ return 'VM'; },
         config
     )), config);
 };
 
-GTxt.MonkeyInTheMiddle._processTxtEmails = function(searchStr, getFrom, getMessageText, config, filterFn) {
+GTxt.MonkeyInTheMiddle._processTxtEmails = function(searchStr, getFrom, getMessageText, getMetadata, config, filterFn) {
     filterFn = filterFn || function(){ return true; };
     var physicalPhoneMessageObjs = [];
     var inboxState = GTxt.Util.getInboxState(searchStr);
@@ -37,24 +43,16 @@ GTxt.MonkeyInTheMiddle._processTxtEmails = function(searchStr, getFrom, getMessa
                 physicalPhoneMessageObjs.push({ message: message, plainMessage: message, text: errorMessage });
             }, config);
         }else if(config.forwardToPhysicalPhone && filterFn(from)){
-            var plainMessage;
+            var plainMessage = JSUtil.ArrayUtil.last(messages.filter(function(m){ return !m.getAttachments().length; }));
+
             var contact = GTxt.Contact.findByNumber(from);
             var text = [contact ? contact.shortId || (from + '(' + contact.createShortId() + ')') : from].concat(messages.map(function(message){
                 var messageDate = message.getDate();
-                var metadata = (JSUtil.DateUtil.diff(messageDate, new Date()) ? JSUtil.DateUtil.toPrettyString(messageDate, true) + '@' : '') +
+                var dateMetadata = (JSUtil.DateUtil.diff(messageDate, new Date()) ? JSUtil.DateUtil.toPrettyString(messageDate, true) + '@' : '') +
                     [messageDate.getHours(), messageDate.getMinutes()].join(':');
+                var metadata = JSUtil.ArrayUtil.compact([dateMetadata, getMetadata(message)]).join(',');
 
-                var messageText = getMessageText(message);
-                if(message.getAttachments().length) {
-                    metadata += ',MMS';
-                    if(messageText === 'MMS Received'){
-                        messageText = '';
-                    }
-                } else {
-                    plainMessage = message;
-                }
-
-                return metadata + (messageText && ('-' + messageText));
+                return JSUtil.ArrayUtil.compact([metadata, getMessageText(message)]).join('-');
             })).join(GTxt.SEPARATOR);
 
             physicalPhoneMessageObjs.push({
