34af7cd1eab4d2319f6e8824d103318408d41801
Garston Tremblay <garstont+github@gmail.com>
2019-03-24 21:09:43 -0600

    [GTxt] compute quick reply contact before processing txt emails of all types (so they will be correctly identified regardless of type)

--- GTxt/MonkeyInTheMiddle.js
+++ GTxt/MonkeyInTheMiddle.js
@@ -1,9 +1,18 @@
 GTxt.MonkeyInTheMiddle = {};
 
 GTxt.MonkeyInTheMiddle.forwardTexts = function(config) {
+    var getNumberFromTxt = function (message) { return GTxt.Voice.parseFromTxt(message).number; };
+    var txtFilterFn = GTxt.Voice.isNotMarketing;
+    var txtInboxState = GTxt.Util.getInboxState('from:' + GTxt.Voice.TXT_DOMAIN + ' subject:' + GTxt.Voice.TXT_SUBJECT);
+    var quickReplyContacts = JSUtil.ArrayUtil.compact(txtInboxState.allThreads.map(function(thread){
+        var from = getNumberFromTxt(thread.getMessages()[0]);
+        return txtFilterFn(from) && from !== config.getPhysicalPhoneContact().number && GTxt.Contact.findByNumber(from);
+    }));
+    var quickReplyContact = quickReplyContacts.length === 1 && quickReplyContacts[0];
+
     GTxt.ReceiverMonkey.txtPhysicalPhone(this._processTxtEmails(
-        'from:' + GTxt.Voice.TXT_DOMAIN + ' subject:' + GTxt.Voice.TXT_SUBJECT,
-        function(message){ return GTxt.Voice.parseFromTxt(message).number; },
+        txtInboxState,
+        getNumberFromTxt,
         function(message){
             var lines = GTxt.Voice.getTxtLines(message, function (line) {
                 return line === 'To respond to this text message, reply to this email or visit Google Voice.' || JSUtil.StringUtil.startsWith(line, 'YOUR ACCOUNT ');
@@ -13,43 +22,37 @@ GTxt.MonkeyInTheMiddle.forwardTexts = function(config) {
             return txt === 'MMS Received' ? '' : txt;
         },
         function(message){ return message.getAttachments().length ? 'MMS' : ''; },
+        quickReplyContact,
         config,
-        GTxt.Voice.isNotMarketing,
-        true
+        txtFilterFn
     ).concat(this._processTxtEmails(
-        'from:' + GTxt.Voice.NO_REPLY_EMAIL + ' subject:' + GTxt.Voice.GROUP_TXT_SUBJECT,
+        GTxt.Util.getInboxState('from:' + GTxt.Voice.NO_REPLY_EMAIL + ' subject:' + GTxt.Voice.GROUP_TXT_SUBJECT),
         function(message){ return GTxt.Voice.getFirstNumberMentioned(message.getSubject()); },
         function(){ return ''; },
         function(){ return 'GM'; },
+        quickReplyContact,
         config
     )).concat(this._processTxtEmails(
-        'from:' + GTxt.Voice.NO_REPLY_EMAIL + ' subject:' + GTxt.Voice.VOICEMAIL_SUBJECT,
+        GTxt.Util.getInboxState('from:' + GTxt.Voice.NO_REPLY_EMAIL + ' subject:' + GTxt.Voice.VOICEMAIL_SUBJECT),
         function(message){
             var subject = message.getSubject();
             return GTxt.Voice.getFirstNumberMentioned(subject) || subject.match(/from (.+) at/)[1];
         },
         function(message){ return GTxt.Voice.getTxtLines(message, function(line){ return line === 'play message'; }).join(' '); },
         function(){ return 'VM'; },
+        quickReplyContact,
         config
     )), config);
 };
 
-GTxt.MonkeyInTheMiddle._processTxtEmails = function(searchStr, getFrom, getMessageText, getMetadata, config, filterFn, canQuickReply) {
+GTxt.MonkeyInTheMiddle._processTxtEmails = function(inboxState, getFrom, getMessageText, getMetadata, quickReplyContact, config, filterFn) {
     filterFn = filterFn || function(){ return true; };
+
     var physicalPhoneMessageObjs = [];
-
-    var inboxState = GTxt.Util.getInboxState(searchStr);
-    var physicalPhoneNumber = config.getPhysicalPhoneContact().number;
-    var quickReplyContacts = canQuickReply ? JSUtil.ArrayUtil.compact(inboxState.allThreads.map(function(thread){
-        var from = getFrom(thread.getMessages()[0]);
-        return filterFn(from) && from !== physicalPhoneNumber && GTxt.Contact.findByNumber(from);
-    })) : [];
-    var quickReplyContact = quickReplyContacts.length === 1 && quickReplyContacts[0];
-
     inboxState.threadMessagesToForward.forEach(function(messages){
         var message = messages[0];
         var from = getFrom(message);
-        if(from === physicalPhoneNumber){
+        if(from === config.getPhysicalPhoneContact().number){
             GTxt.SenderMonkey.txtContacts(messages, quickReplyContact, getMessageText, function(errorMessage){
                 physicalPhoneMessageObjs.push({ message: message, plainMessage: message, text: errorMessage });
             }, config);
@@ -59,7 +62,7 @@ GTxt.MonkeyInTheMiddle._processTxtEmails = function(searchStr, getFrom, getMessa
             var fromStr = from;
             var contact = GTxt.Contact.findByNumber(from);
             if(contact){
-                var quickReplyNotation = quickReplyContact ? '!' : '';
+                var quickReplyNotation = contact === quickReplyContact ? '!' : '';
                 fromStr = contact.shortId ? contact.shortId + quickReplyNotation : from + '(' + contact.createShortId() + quickReplyNotation + ')';
             }
 
