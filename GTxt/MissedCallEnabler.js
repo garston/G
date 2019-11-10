10f8c5bfd5421c2e9aa13d48bbe6c581cf395729
Garston Tremblay <garstont+github@gmail.com>
2019-11-09 19:42:22 -0700

    [GTxt] don't double-toggle forwardToPhysicalPhone when receive multiple emails for same missed call

--- GTxt/MissedCallEnabler.js
+++ GTxt/MissedCallEnabler.js
@@ -4,8 +4,10 @@ GTxt.MissedCallEnabler.changeEnabled = function(config) {
     GmailApp.search('from:' + GTxt.Voice.NO_REPLY_EMAIL + ' is:unread subject:' + GTxt.Voice.MISSED_CALL_SUBJECT).
         reduce(function (messages, thread){ return messages.concat(GASton.Mail.getMessagesAfterLatestMessageSentByScript(thread)); }, []).
         filter(function(message){ return !message.isInTrash() && message.isUnread() && this._isToggleEnabledRequest(message, config); }, this).
-        forEach(function(message){
-            GASton.Mail.forward(message, config.toggleForwardToPhysicalPhone() + ' ' + SpreadsheetApp.getActiveSpreadsheet().getUrl(), Session.getActiveUser().getEmail());
+        forEach(function(message, i, msgs){
+            if(!i || msgs[i-1].getDate().getTime() !== message.getDate().getTime()){
+                GASton.Mail.forward(message, config.toggleForwardToPhysicalPhone() + ' ' + SpreadsheetApp.getActiveSpreadsheet().getUrl(), Session.getActiveUser().getEmail());
+            }
             GASton.Mail.markRead(message);
         });
 };
