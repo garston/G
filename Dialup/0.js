ec14fa08402afcbdc9477bc5065040ad29c3faed
Garston Tremblay <garstont+github@gmail.com>
2022-08-07 14:44:50 +0200

    [Dialup] add security

--- Dialup/0.js
+++ Dialup/0.js
@@ -1,5 +1,9 @@
 Dialup = {};
 
 function doGet() {
-    return ContentService.createTextOutput(Dialup.MailRenderer.generateHtml().join(''));
+    return ContentService.createTextOutput(
+        JSUtil.ArrayUtil.sum(GmailApp.search('from:voice-noreply@google.com in:inbox subject:"missed call from Home"').map(t => t.getMessages().filter(m => m.isInInbox()).length)) % 2 ?
+        Dialup.MailRenderer.generateHtml().join('') :
+        '0'
+    );
 }
--- Dialup/test/GRTest.js
+++ Dialup/test/GRTest.js
@@ -1,12 +1,16 @@
 GRTest.describeApp('Dialup', {
-    ['in:inbox']: 'inbox'
+    ['in:inbox']: 'inbox',
+    ['from:voice-noreply@google.com in:inbox subject:"missed call from Home"']: 'missedCalls'
 }, () => {
     GRTest.describeFn('doGet', () => {
+        GRTest.it('renders 0 when not enabled', [], {}, [], '0');
+
         GRTest.it('renders inbox', [], {
             inbox: [[
                 {getBody: () => 'b1', getFrom: () => 'email', getSubject: () => 's1', isUnread: () => false},
                 {getBody: () => 'b2', getFrom: () => 'f l <email>', isUnread: () => true}
-            ]]
+            ]],
+            missedCalls: [[{isInInbox: () => false}, {isInInbox: () => true}]]
         }, [], `<table><tr><th></th><th>s1</th><th></th></tr><tr><td>email</td><td>b1</td><td>${GRTest.Util.nowStr}</td></tr><tr style="font-weight: bold"><td>f l</td><td>b2</td><td>${GRTest.Util.nowStr}</td></tr></table>`);
     });
 });
