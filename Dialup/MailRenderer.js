a174d8c2551e0b140b29afc5d09fb25c8c6f1035
Garston Tremblay <garstont+github@gmail.com>
2022-12-30 17:01:24 -0500

    [Dialup] add button to remove Thread ID from rendered list

--- Dialup/MailRenderer.js
+++ Dialup/MailRenderer.js
@@ -1,12 +1,21 @@
 Dialup.MailRenderer = {};
 Dialup.MailRenderer.generateHtml = function(threadMessages, bodyLength){
     const threads = threadMessages.map(([msg]) => msg.getThread());
+    const idThreadIds = 't';
     return [
-        this._wrapEach('div', [`Time: ${Date.now()}`, `Thread IDs: ${threads.map(t => t.getId()).join(',')}`]).join(''),
+        this._wrapEach('div', [
+            `Time: ${Date.now()}`,
+            `Thread IDs: <span id="${idThreadIds}">${threads.map(t => t.getId()).join(',')}</span>`
+        ]).join(''),
         ...threadMessages.map((messages, i) => [
             '<table>',
                 '<tr>',
-                    ...this._wrapEach('th', ['', JSUtil.StringUtil.escapeHTML(threads[i].getFirstMessageSubject()), '', threads[i].getId()]),
+                    ...this._wrapEach('th', [
+                        `<input onclick="var e = document.getElementById('${idThreadIds}'); e.innerHTML = e.innerHTML.replace(/${threads[i].getId()},?/, '')" type="button" />`,
+                        JSUtil.StringUtil.escapeHTML(threads[i].getFirstMessageSubject()),
+                        '',
+                        threads[i].getId()
+                    ]),
                 '</tr>',
                 ...messages.map(m => [
                     `<tr${m.isUnread() ? ' style="font-weight: bold"' : ''}>`,
